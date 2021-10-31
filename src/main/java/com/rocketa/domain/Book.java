5fbc75d7f9d8f6c50feb3e646d07e9f48920e4f8
rocketpowers <rocketpowerswillian@gmail.com>
2021-10-31 20:52:02 -0300

    jpa-repositoty implements

--- src/main/java/com/rocketa/domain/Book.java
+++ src/main/java/com/rocketa/domain/Book.java
@@ -1,12 +1,32 @@
 package com.rocketa.domain;
 
-public class Book {
+import java.io.Serializable;
+
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book  implements Serializable{
+
+	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
 	private String title;
 	private String author_name;
 	private String text;
 	
+	@ManyToOne
+	@JoinColumn(name = "category_id")
 	private Category category;
 
 }
--- src/main/java/com/rocketa/domain/Category.java
+++ src/main/java/com/rocketa/domain/Category.java
@@ -1,7 +1,14 @@
 package com.rocketa.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,15 +16,22 @@ import lombok.NoArgsConstructor;
 
 
 
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Category {
+public class Category implements Serializable{
 
+	private static final long serialVersionUID = 1L;
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 	private String description;
 	
+	@OneToMany
 	private List<Book> books = new ArrayList<>();
 
 }
