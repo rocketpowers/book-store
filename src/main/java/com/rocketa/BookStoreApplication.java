c3596812499afd3bc5bb748a6f00c7f31328faf6
rocketpowers <rocketpowerswillian@gmail.com>
2021-11-01 10:03:54 -0300

    fixed failures-h2 implemented

--- src/main/java/com/rocketa/BookStoreApplication.java
+++ src/main/java/com/rocketa/BookStoreApplication.java
@@ -1,13 +1,43 @@
 package com.rocketa;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.rocketa.domain.Book;
+import com.rocketa.domain.Category;
+import com.rocketa.repository.BookRepository;
+import com.rocketa.repository.CategoryRepository;
+
 @SpringBootApplication
-public class BookStoreApplication {
+public class BookStoreApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	
+	@Autowired
+	private BookRepository bookRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookStoreApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		 Category cat1 = new Category(null, "Informatica", "ti books");
+		 
+		 Book b1 = new Book(null,"clean code", "robert marchas", "sem texto",cat1);
+		
+		 
+		 cat1.getBooks().addAll(Arrays.asList(b1));
+		 
+		 this.categoryRepository.saveAll(Arrays.asList(cat1));
+		 this.bookRepository.saveAll(Arrays.asList(b1));
+		 
 	}
 
 }
--- src/main/java/com/rocketa/domain/Book.java
+++ src/main/java/com/rocketa/domain/Book.java
@@ -1,32 +1,100 @@
 package com.rocketa.domain;
 
 import java.io.Serializable;
-
+import java.util.Objects;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-
 @Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class Book  implements Serializable{
+public class Book implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String title;
 	private String author_name;
 	private String text;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "category_id")
 	private Category category;
+
+	public Book() {
+		super();
+
+	}
+
+	public Book(Integer id, String title, String author_name, String text, Category category) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.author_name = author_name;
+		this.text = text;
+		this.category = category;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor_name() {
+		return author_name;
+	}
+
+	public void setAuthor_name(String author_name) {
+		this.author_name = author_name;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		return Objects.equals(id, other.id);
+	}
 
 }
